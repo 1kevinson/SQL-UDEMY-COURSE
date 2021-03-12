8a08ff60917bf518d460b36edb3f951e49406b3f
Arsene Kevin <kevinkouomeu@icloud.com>
2021-03-12 10:02:17 +0100

    exercice 5

--- section-3.sql
+++ section-3.sql
@@ -11,3 +11,10 @@ WHERE birth_date > 1986
 SELECT name
 FROM people
 WHERE birth_date IN (1988,1989,1990) AND name LIKE 'P%';
+
+----------------
+-- EXERCICE 5 --
+----------------
+SELECT UPPER(name) AS name, email, birth_date, (2021 - birth_date) AS age
+FROM people
+WHERE (2021 - birth_date) > 35;
